8fd9fa229b50db6949cf9c8524b4310ae5d4f0dd
xwang1000 <23066125+xwang1000@users.noreply.github.com>
2019-03-19 21:42:15 +0000

    Change names

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,7 +5,7 @@ puts "----------"
 
 # Your code goes below here ...
 richmond = Store.create(name: 'richmond', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
-burnaby  = Store.create(name: 'burnaby', annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true)
-gastown  = Store.create(name: 'gastown', annual_revenue: 190_000, mens_apparel: true, womens_apparel: false)
+burnaby  = Store.create(name: 'Burnaby', annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true)
+gastown  = Store.create(name: 'Gastown', annual_revenue: 190_000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
