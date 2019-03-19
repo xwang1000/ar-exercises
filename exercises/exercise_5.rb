7b5c72d89a0fea748eed646b8b78efcafdfd305d
xwang1000 <23066125+xwang1000@users.noreply.github.com>
2019-03-19 21:51:55 +0000

    Complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+totalRevenue = Store.sum(:annual_revenue)
+storeCount = Store.count
+averageRevenue = totalRevenue / storeCount
+
+@number_stores_more_than_1m = Store.where('annual_revenue > ?', 1_000_000).size
+
+puts "The company's annual revenue from #{storeCount} stores: #{totalRevenue}"
+puts "Average annual revenue for each store: #{averageRevenue}"
+puts "Number of stores generating more than $1M annually: #{@number_stores_more_than_1m}"
