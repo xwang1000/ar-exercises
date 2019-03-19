7d78ca00a39f072c82a14393c5cdb178bf4dec61
xwang1000 <23066125+xwang1000@users.noreply.github.com>
2019-03-19 21:20:39 +0000

    Complete exercise 1 and 2

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 source "https://rubygems.org"
 
 gem 'pry' # for debugging
-gem 'pg'
 gem 'activerecord'
+gem 'pg', '~> 0.18.4'
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = 'Richmond'
+@store1.save
+
+puts @store1.name
