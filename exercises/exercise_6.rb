4dd5472e8f8c472cbb313aba210211106cf629f6
xwang1000 <23066125+xwang1000@users.noreply.github.com>
2019-03-19 23:16:56 +0000

    Complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Nancy", last_name: "Shmancy", hourly_rate: 600)
+@store1.employees.create(first_name: "Bash", last_name: "Shellsy", hourly_rate: 12)
+@store1.employees.create(first_name: "Scott", last_name: "Christian", hourly_rate: 30)
+@store2.employees.create(first_name: "Pedro", last_name: "Dedecon", hourly_rate: 300)
+@store2.employees.create(first_name: "Maria", last_name: "Conner", hourly_rate: 80)
+
+puts "There are #{Employee.count} employees."
